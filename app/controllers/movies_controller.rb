89feae8bc2c04883acddd0481bc7722968d4a8b9
a17100037 <17100037@lums.edu.pk>
2016-02-27 14:16:02 +0000

    commit-3

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,7 +10,7 @@ class MoviesController < ApplicationController
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
   end
-
+  
   def index
     @hilite = "hilite"
     if params.has_key?(:sort)
@@ -40,6 +40,29 @@ class MoviesController < ApplicationController
     @movie.update_attributes!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
+  end
+
+  def updates
+    
+  end
+  
+  def updatess
+    updateVariables = params.require(:movie).permit(:existingTitle, :title, :rating, :release_date)
+    @movie = Movie.find_by_title(updateVariables[:existingTitle])
+    if @movie
+      if updateVariables[:title] != ""
+        @movie.update_attribute(:title, updateVariables[:title])
+      end
+      if updateVariables[:rating] != ""
+        @movie.update_attribute(:rating, updateVariables[:rating])
+      end
+      if updateVariables[:release_date] != ""
+        @movie.update_attribute(:release_date, updateVariables[:release_date])
+      end
+      redirect_to movie_path(@movie)
+    else
+      redirect_to movies_path
+    end
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'movies#index'
+  get 'movies/update' => 'movies#updates'
+  put 'movies(.:format)' => 'movies#updatess'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
